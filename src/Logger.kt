e3c20ba6febaf62af4c47f8db99eaf9776ad7cc5
qoocrab <qoocrab@gmail.com>
2023-02-22 11:16:58 +0900

    feat: 로그용 객체 추가

---
+++ src/Logger.kt
@@ -0,0 +1,11 @@
+package src
+
+object Logger {
+    fun i(message: String) {
+        println("Log Info: $message")
+    }
+
+    fun e(errorMessage: String) {
+        println("Log Error: $errorMessage")
+    }
+}
